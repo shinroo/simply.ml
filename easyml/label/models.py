b81226629fbe61a0f2116dec17b220b50d21557b
Robert Focke <robert.focke96@gmail.com>
2017-07-11 15:24:28 +0200

    django init

---
+++ easyml/label/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# Usage of built in user class
+# u = User.objects.get(username='fsmith')
+# freds_department = u.employee.department
+
+class TechnicalUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class EndUser(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Dataset(models.Model):
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	owner = models.OneToOneField(TechnicalUser)
+	elements = models.OneToManyField(Dataelement, on_delete=models.CASCADE)
+
+class Dataelement(models.Model):
+	data = models.CharField(max_length=1000)
+	dataset = models.OneToOneField(Dataset)
