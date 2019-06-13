2b359ce2f7f90dc7176dec2f59a4ebdf42513795
Robert Focke <robert_focke@yahoo.com>
2019-06-13 18:05:57 +0200

    refactor to plugins/{functionality-group}/{job-type}#

--- src/platform/plugin_manifest.py
+++ src/platform/plugin_manifest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import plugins.time_series.fbprophet_forecasting as historical_data_forecasting
+import plugins.time_series.historical_data_forecasting.fbprophet_forecasting as historical_data_forecasting
 
 plugin_manifest = {
 	"historical-data-forecasting": {
