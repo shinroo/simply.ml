c4df5f388a868b8f9339a842a360e6e239866078
Robert Focke <robert_focke@yahoo.com>
2019-06-13 18:09:09 +0200

    update historical-data-forecasting docs in plugin manifest

--- src/platform/plugin_manifest.py
+++ src/platform/plugin_manifest.py
@@ -5,6 +5,6 @@ import plugins.time_series.historical_data_forecasting.fbprophet_forecasting as 
 plugin_manifest = {
 	"historical-data-forecasting": {
 		"handler": historical_data_forecasting.handle_request,
-		"docs": "https://github.com/shinroo/simply.ml/blob/master/src/platform/plugins/time_series/README.md"
+		"docs": "https://github.com/shinroo/simply.ml/blob/master/src/platform/plugins/time_series/historical_data_forecasting/README.md"
 	}
 }
